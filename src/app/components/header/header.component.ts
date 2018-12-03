6a624ace9b22dcc76af556c7be6406f7cda40b3a
carlos sk <skyrko.carlos.sk@gmail.com>
2018-12-02 19:55:01 -0600

    components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-    constructor() {
-
+    constructor(public auth0: AuthService) {
+        this.auth0.handleAuthentication();
+    }
+    login() {
+      this.auth0.login();
+    }
+    logout() {
+      this.auth0.logout();
     }
 }
